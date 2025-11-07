938a9f1a4909a97e7c2ad878cbf1391b33ef9051
Ashutoshbirje <ashutoshbirje4@gmail.com>
2025-11-07 21:23:32 +0530

    ok

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config(); e
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const tokens = process.env.GITHUB_TOKENS ? process.env.GITHUB_TOKENS.split(",") : [];
+
+app.get("/token", (req, res) => {
+  res.json({
+    message: "Secure API",
+    count: tokens.length,
+    tokens: tokens.map(() => "****PROTECTED****") 
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
